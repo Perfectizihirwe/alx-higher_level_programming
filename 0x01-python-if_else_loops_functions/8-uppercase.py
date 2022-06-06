6d3a6327fbc2285eda83b72ce73b397598ed9add
Perfectizihirwe <perfectizihirwe@gmail.com>
2022-06-06 12:38:12 +0100

    update readme

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for ch in str:
+        """Change to uppercase character."""
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            ch = chr(ord(ch) - 32)
+        print("{}".format(ch), end="")
+    print()
