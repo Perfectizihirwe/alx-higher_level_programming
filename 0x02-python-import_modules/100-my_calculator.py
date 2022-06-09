030ac7d071f848e00c871750bd0bb2b5b5a250d9
Perfectizihirwe <perfectizihirwe@gmail.com>
2022-06-09 19:49:46 +0200

    update readme

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Handle basic arithemetic."""
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    argc = len(argv) - 1
+
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+
+    if op not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, op, b, ops[op](a, b)))
